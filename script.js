69d79df7086606ea404da2a46c3fc825f5aae485
Ollie-Judge <ollie.judge@hotmail.com>
2024-01-31 12:02:00 +0000

    added and removed an opacity on drag

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const containers = document.querySelectorAll(".draggableItemContainer");
 
 draggables.forEach((draggable) => {
   draggable.addEventListener("dragstart", () => {
+    draggable.classList.add("currentlyDragging");
     console.log("start");
+  });
+
+  draggable.addEventListener("dragend", () => {
+    draggable.classList.remove("currentlyDragging");
+    console.log("end");
   });
 });
